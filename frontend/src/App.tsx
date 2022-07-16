7a15a9a794e11314ba28fe65f5c77df3b25e9018
Helder Moreira <heldermxa@gmail.com>
2022-07-16 17:10:30 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Oi</h1>
+    <>
+      <h1>Oi</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
